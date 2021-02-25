b879d3b8ca1eaa982e21be96e050f084789b2e0d
andanao <47489123+andanao@users.noreply.github.com>
2021-02-25 07:41:43 -0500

    removed ivy prescient
    somehow not on melpa ??

--- init.el
+++ init.el
@@ -183,14 +183,14 @@
   :init
   (ivy-rich-mode 1))
 
-(use-package ivy-prescient
-  :after counsel
-  :custom
-  (ivy-prescient-enable-filtering nil)
-  :config
-  ;; Uncomment the following line to have sorting remembered across sessions!
-  (prescient-persist-mode 1)
-  (ivy-prescient-mode 1))
+;    (use-package ivy-prescient
+;      :after counsel
+;      :custom
+;      (ivy-prescient-enable-filtering nil)
+;      :config
+      ;; Uncomment the following line to have sorting remembered across sessions!
+;      (prescient-persist-mode 1)
+;      (ivy-prescient-mode 1))
 
 (use-package helpful
   :ensure t
