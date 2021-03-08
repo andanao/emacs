8b828fb2539e0aa972dedadec08026daea4ed049
andanao <47489123+andanao@users.noreply.github.com>
2021-03-07 20:10:58 -0500

    removed hide dotfiles

--- init.el
+++ init.el
@@ -615,8 +615,9 @@ One for writing code and the other for reading articles."
     (evil-collection-define-key 'normal 'dired-mode-map
       "h" 'dired-single-up-directory
       "l" 'dired-single-buffer)
-      
-     (setq delete-by-moving-to-trash t))
+
+     (setq delete-by-moving-to-trash t)
+     (setq dired-hide-details-mode t))
 
 (use-package dired-single
     :commands (dired dired-jump))
@@ -624,16 +625,10 @@ One for writing code and the other for reading articles."
 (use-package all-the-icons-dired
     :hook (dired-mode . all-the-icons-dired-mode))
 
-(use-package dired-open
-    :commands (dired dired-jump)
-    :config
-    ;; Doesn't work as expected!
-    ;;(add-to-list 'dired-open-functions #'dired-open-xdg t)
-    (setq dired-open-extensions '(("png" . "feh")
-                                  ("mkv" . "mpv"))))
-
-(use-package dired-hide-dotfiles
-    :hook (dired-mode . dired-hide-dotfiles-mode)
-    :config
-    (evil-collection-define-key 'normal 'dired-mode-map
-      "H" 'dired-hide-dotfiles-mode))
+;(use-package dired-open
+;    :commands (dired dired-jump)
+;    :config
+;    ;; Doesn't work as expected!
+;    ;;(add-to-list 'dired-open-functions #'dired-open-xdg t)
+;    (setq dired-open-extensions '(("pdf" . "feh")
+				 ;; ("mkv" . "mpv"))))
