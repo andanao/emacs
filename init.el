4caebfce64e9ff6281d0ca139102bbe9541f3d2f
Adrian Danao-Schroeder <47489123+andanao@users.noreply.github.com>
2021-08-02 14:03:56 -0400

    Fix org agenda on work and computer files

--- init.el
+++ init.el
@@ -383,8 +383,6 @@ One for writing code and the other for reading articles."
 (if (string= efs/computer-id "work") 
     (setq org-agenda-files (list 
 	 (concat efs/user-dir-org "work.org")
-	 (concat efs/user-dir-org "personal.org")
-	 (concat efs/user-dir-org "habits.org")
 	 (concat efs/user-dir-org "dates.org")
 	 (concat efs/user-dir-org "inbox.org")
 	 ))
