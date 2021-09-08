48fe5796af0299032b0a96b9563723b127a94803
Adrian Danao-Schroeder <47489123+andanao@users.noreply.github.com>
2021-09-08 14:38:59 -0400

    don't include subscripts in pretty entities

--- init.el
+++ init.el
@@ -361,6 +361,7 @@ One for writing code and the other for reading articles."
 	(setq org-log-done 'time)
 	(setq org-log-into-drawer t)
 	(setq org-pretty-entities t)
+	(setq org-pretty-entities-include-sub-superscripts nil) 
 	(setq org-hidden-keywords '(title) )
 
 	(setq org-hide-emphasis-markers t)
