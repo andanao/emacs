ca4c5ae27f791301120d2b3f50ccc60ed7c0684f
Adrian Danao-Schroeder <47489123+andanao@users.noreply.github.com>
2021-09-21 17:05:48 -0400

    remove loading work message

--- init.el
+++ init.el
@@ -811,5 +811,4 @@ same directory as the org-buffer and insert a link to this file."
   (setq x-select-request-type '(UTF8_STRING COMPOUND_TEXT TEXT STRING)))
 
 (if (string= efs/computer-id "work") 
-    ((message "loading work.el")
-    (load-file (concat efs/user-dir-org "work.el"))))
+    ((load-file (concat efs/user-dir-org "work.el"))))
