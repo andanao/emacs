57ae3f038c39cc3dda56cee1813ebc79bbe3d260
Adrian Danao-Schroeder <47489123+andanao@users.noreply.github.com>
2021-10-13 21:06:54 -0400

    dark theme now shamelessly stolen doom molokai (for easier changes)

--- ads-dark-theme.el
+++ ads-dark-theme.el
@@ -1,120 +1,160 @@
-(deftheme ads-dark
-  "Created 2021-10-12 testing stolen from vscode emacs theme")
+(require 'doom-themes)
 
-(custom-theme-set-faces
- 'ads-dark
- '(cursor ((t (:background "#fff" :foreground "#002b36" :inverse-video t :box (:line-width 1 :color "#f38518")))))
- '(fixed-pitch ((t (:family "Monospace"))))
- '(variable-pitch ((((type w32)) (:foundry "outline" :family "Arial")) (t (:family "Sans Serif"))))
- '(escape-glyph ((t (:foreground "#6c71c4"))))
- '(homoglyph ((((background dark)) (:foreground "cyan")) (((type pc)) (:foreground "magenta")) (t (:foreground "brown"))))
- '(minibuffer-prompt ((t (:foreground "#839496" :background "#000000"))))
- '(highlight ((t (:background "#073642"))))
- '(region ((t (:background "#fff" :foreground "#000"))))
- '(shadow ((t (:foreground "#586e75"))))
- '(secondary-selection ((t (:background "#073642"))))
- '(trailing-whitespace ((t (:background "#7ca668"))))
- '(font-lock-builtin-face ((t (:weight bold :slant normal :foreground "#839496"))))
- '(font-lock-comment-delimiter-face ((t (:foreground "#7ca668" :slant normal))))
- '(font-lock-comment-face ((t (:foreground "#7ca668" :slant normal))))
- '(font-lock-constant-face ((t (:foreground "#268bd2" :weight normal))))
- '(font-lock-doc-face ((t (:foreground "#7ca668" :slant normal))))
- '(font-lock-function-name-face ((t (:foreground "#DCDCAA"))))
- '(font-lock-keyword-face ((t (:foreground "#268bd2" :weight normal))))
- '(font-lock-negation-char-face ((t (:weight bold :foreground "#b58900"))))
- '(font-lock-preprocessor-face ((t (:foreground "#268bd2"))))
- '(font-lock-regexp-grouping-backslash ((t (:weight bold :foreground "#859900"))))
- '(font-lock-regexp-grouping-construct ((t (:weight bold :foreground "#b58900"))))
- '(font-lock-string-face ((t (:foreground "#ce9178"))))
- '(font-lock-type-face ((t (:foreground "#b58900"))))
- '(font-lock-variable-name-face ((t (:foreground "#9cdcfe"))))
- '(font-lock-warning-face ((t (:weight bold :foreground "#ff69b4" :inherit (error)))))
- '(button ((t (:underline (:color foreground-color :style line)))))
- '(link ((t (:underline t))))
- '(link-visited ((t (:weight normal :underline (:color foreground-color :style line) :foreground "#b58900"))))
- '(fringe ((t ( :foreground "#ffffff" :background "#000000" :box (:line-width 1 :color "#9CDCFE")))))
- '(header-line ((t (:underline (:color "#f38518" :style line) :overline nil :box (:line-width 1 :color "#f38518" :style unspecified) :foreground "#ffffff" :background "#000000"))))
- '(tooltip ((t (:foreground "#DEB542" :background "#7B6000" :inherit (variable-pitch)))))
- '(mode-line ((t (:background "#000" :foreground "#fff"  :overline "#9CDCFE" :underline "#9CDCFE"))))
- '(mode-line-inactive ((t (:background "#000" :foreground "#fff"  :overline "#9CDCFE" :underline "#9CDCFE"))))
- '(mode-line-buffer-id ((t (:background "black" :foreground "#ffffff"  :weight bold :overline "#f38518" :underline "#f38518" :box (:line-width 1 :color "#f38518")))))
- '(mode-line-buffer-id-inactive ((t (:background "black" :foreground "#ffffff" :weight bold ))))
- '(mode-line-emphasis ((t (:weight bold))))
- '(mode-line-highlight ((((class color) (min-colors 88)) (:box (:line-width 2 :color "grey40" :style released-button))) (t (:inherit (highlight)))))
- '(isearch ((t (:weight normal :box (:line-width 2 :color "#f38518") :foreground "#000000" :background "#848484"))))
- '(isearch-fail ((t (:weight bold :foreground "#dc322f" :background "#002b36"))))
- '(lazy-highlight ((t (:box (:line-width 1 :color "#f38518")))))
- '(match ((t (:weight bold :foreground "#93a1a1" :background "#073642"))))
- '(next-error ((t (:inherit (region)))))
- '(query-replace ((((class color) (min-colors 89)) (:background "#333333"))))
- '(hl-line ((t (:background "black" :box (:line-width 1 :color "#f38518")))))
- '(rainbow-delimiters-depth-4-face ((t (:foreground "white"))))
- '(sp-show-pair-match-face ((t (:box (:line-width 1 :color "white")))))
- '(rainbow-delimiters-base-face ((default (:inherit nil))))
- '(rainbow-delimiters-depth-5-face ((t (:foreground "white"))))
- '(rainbow-delimiters-depth-1-face ((t (:foreground "white"))))
- '(rainbow-delimiters-depth-2-face ((t (:foreground "white"))))
- '(rainbow-delimiters-depth-3-face ((t (:foreground "white"))))
- '(rainbow-delimiters-depth-6-face ((t (:foreground "white"))))
- '(window-divider ((t (:foreground "#9cdcfe" :width condensed))))
- '(tool-bar ((t (:background "#fff" :foreground "#000" :box (:line-width 1 :style released-button)))))
- '(window-divider-last-pixel ((t (:foreground "gray40"))))
- '(border ((t (:foreground "#9cdcfe"))))
- '(org-level-1 ((t (:foreground "#b180d7"))))
- '(org-level-2 ((t (:foreground "#4EC9B0"))))
- '(org-level-3 ((t (:foreground "#DCDCAA"))))
- '(org-level-4 ((t (:inherit (outline-4)))))
- '(org-level-5 ((t (:inherit (outline-5)))))
- '(org-level-6 ((t (:inherit (outline-6)))))
- '(org-level-7 ((t (:inherit (outline-7)))))
- '(org-level-8 ((t (:inherit (outline-8)))))
- '(custom-link ((t (:foreground "#9cdcfe" :underline t))))
- '(org-headline-todo ((((class color) (min-colors 16) (background light)) (:foreground "Red4")) (((class color) (min-colors 16) (background dark)) (:foreground "Pink2")) (((class color) (min-colors 8) (background light)) (:bold t))))
- '(org-checkbox-statistics-todo ((t (:foreground "#DBABA7"))))
- '(org-todo ((t (:inherit bold :foreground "#D8ABA7" :box (:line-width 1 :color "#D8ABA7")))))
- '(markdown-inline-code-face ((t (:foreground "#e2c08d"))))
- '(org-document-title ((t (:inherit font-lock-keyword-face :weight bold :height 1.0))))
- '(org-block ((t (:inherit nil :extend t :foreground "#b180d7"))))
- '(default ((t (:inherit nil :stipple nil :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 98 :width normal :foundry "PfEd" :family "DejaVu Sans Mono"))))
- '(ediff-odd-diff-A ((t (:extend t :box (:line-width 2 :color "grey75" :style released-button)))))
- '(ediff-odd-diff-B ((t (:extend t :box (:line-width 2 :color "LightGrey" :style released-button)))))
- '(ediff-odd-diff-C ((t (:extend t :box (:line-width 2 :color "Grey" :style released-button)))))
- '(ediff-fine-diff-A ((t (:box (:line-width 2 :color "#aa2222" :style released-button)))))
- '(ediff-fine-diff-B ((t (:box (:line-width 2 :color "#22aa22" :style released-button)))))
- '(ediff-fine-diff-C ((t (:box (:line-width 2 :color "#aaaa22" :style released-button)))))
- '(ediff-even-diff-A ((t (:extend t :box (:line-width 2 :color "light grey" :style released-button)))))
- '(ediff-even-diff-B ((t (:extend t :box (:line-width 2 :color "Grey" :style released-button)))))
- '(ediff-even-diff-C ((t (:extend t :box (:line-width 2 :color "light grey" :style released-button)))))
- '(ediff-current-diff-A ((t (:extend t :box (:line-width 2 :color "#553333" :style released-button)))))
- '(ediff-current-diff-B ((t (:extend t :box (:line-width 2 :color "#335533" :style released-button)))))
- '(ediff-current-diff-C ((t (:extend t :box (:line-width 2 :color "#888833" :style released-button)))))
- '(ediff-odd-diff-Ancestor ((t (:extend t :box (:line-width 2 :color "gray40" :style released-button)))))
- '(ediff-fine-diff-Ancestor ((t (:box (:line-width 2 :color "#009591" :style released-button)))))
- '(ediff-even-diff-Ancestor ((t (:extend t :box (:line-width 2 :color "Grey" :style released-button)))))
- '(ediff-current-diff-Ancestor ((t (:extend t :box (:line-width 2 :color "#004151" :style released-button)))))
- '(mu4e-unread-face ((t (:foreground "#b180d7"))))
- '(mu4e-header-face ((t (:inherit (default)))))
- '(mu4e-flagged-face ((t (:weight bold :inherit (font-lock-constant-face)))))
- '(mu4e-header-value-face ((t (:inherit (font-lock-type-face)))))
- '(mu4e-view-body-face ((t (:inherit (default)))))
- '(mu4e-header-highlight-face ((t (:box (:line-width 1 :color "#f38518")))))
- '(mu4e-header-key-face ((t (:foreground "#b180d7" :weight bold))))
- '(mu4e-header-title-face ((t (:inherit (font-lock-type-face)))))
- '(mu4e-link-face ((t (:inherit (link)))))
- '(web-mode-javascript-string-face ((t (:inherit (web-mode-string-face)))))
- '(web-mode-string-face ((t (:foreground "#ce9178"))))
- '(web-mode-javascript-comment-face ((t (:foreground "#7ca668"))))
- '(web-mode-comment-face ((t (:foreground "#7ca668"))))
- '(web-mode-html-tag-face ((t (:inherit font-lock-constant-face))))
- '(web-mode-html-tag-custom-face ((t (:inherit web-mode-html-tag-face :slant italic))))
- '(web-mode-html-attr-name-face ((t (:inherit font-lock-variable-name-face))))
- '(js2-function-param ((t (:inherit font-lock-variable-name-face))))
- '(js2-object-property ((t (:inherit font-lock-variable-name-face))))
- '(js2-object-property-access ((t (:inherit (js2-object-property)))))
- '(js2-external-variable ((t (:foreground "#4EC9B0"))))
- '(markdown-inline-code-face ((t (:foreground "#e2c08d"))))
- '(js2-function-call ((t (:inherit font-lock-function-name-face))))
-)
 
-(provide-theme 'ads-dark)
+(def-doom-theme ads-dark
+  "A dark, vibrant theme inspired by Textmate's Monokai."
 
+  ;; name        gui       256       16
+  ((bg         '("#1c1e1f" "black"   "black"        ))
+   (fg         '("#d6d6d4" "#dfdfdf" "brightwhite"  ))
+
+   ;; These are off-color variants of bg/fg, used primarily for `solaire-mode',
+   ;; but can also be useful as a basis for subtle highlights (e.g. for hl-line
+   ;; or region), especially when paired with the `doom-darken', `doom-lighten',
+   ;; and `doom-blend' helper functions.
+   (bg-alt     '("#222323" "black"   "black"        ))
+   (fg-alt     '("#556172" "#4d4d4d" "white"        ))
+
+   ;; These should represent a spectrum from bg to fg, where base0 is a starker
+   ;; bg and base8 is a starker fg. For example, if bg is light grey and fg is
+   ;; dark grey, base0 should be white and base8 should be black.
+   (base0      '("#1B2229" "black"   "black"        ))
+   (base1      '("#151617" "#101010" "brightblack"  ))
+   (base2      '("#1d1f20" "#191919" "brightblack"  ))
+   (base3      '("#2d2e2e" "#252525" "brightblack"  ))
+   (base4      '("#4e4e4e" "#454545" "brightblack"  ))
+   (base5      '("#555556" "#6b6b6b" "brightblack"  ))
+   (base6      '("#767679" "#7b7b7b" "brightblack"  ))
+   (base7      '("#cfc0c5" "#c1c1c1" "brightblack"  ))
+   (base8      '("#ffffff" "#ffffff" "brightwhite"  ))
+
+   (grey       '("#525254" "#515154" "brightblack"  ))
+   (red        '("#e74c3c" "#e74c3c" "red"          ))
+   (orange     '("#fd971f" "#fd971f" "brightred"    ))
+   (green      '("#b6e63e" "#b6e63e" "green"        ))
+   (teal       green)
+   (yellow     '("#e2c770" "#e2c770" "yellow"       ))
+   (blue       '("#268bd2" "#2686D6" "brightblue"   ))
+   (dark-blue  '("#727280" "#727280" "blue"         ))
+   (magenta    '("#fb2874" "#fb2874" "magenta"      ))
+   (violet     '("#9c91e4" "#9c91e4" "brightmagenta"))
+   (cyan       '("#66d9ef" "#66d9ef" "brightcyan"   ))
+   (dark-cyan  '("#8fa1b3" "#8FA1B3" "cyan"         ))
+
+   ;; These are the "universal syntax classes" that doom-themes establishes.
+   ;; These *must* be included in every doom themes, or your theme will throw an
+   ;; error, as they are used in the base theme defined in doom-themes-base.
+   (highlight      orange)
+   (vertical-bar   (doom-lighten bg 0.1))
+   (selection      base5)
+   (builtin        orange)
+   (comments       (if doom-molokai-brighter-comments violet base5))
+   (doc-comments   (if doom-molokai-brighter-comments
+                       (doom-lighten violet 0.1)
+                     (doom-lighten base5 0.25)))
+   (constants      orange)
+   (functions      green)
+   (keywords       magenta)
+   (methods        cyan)
+   (operators      violet)
+   (type           cyan)
+   (strings        yellow)
+   (variables      orange)
+   (numbers        violet)
+   (region         base4)
+   (error          red)
+   (warning        yellow)
+   (success        green)
+   (vc-modified    cyan)
+   (vc-added       (doom-darken green 0.15))
+   (vc-deleted     red)
+
+   ;; These are extra color variables used only in this theme; i.e. they aren't
+   ;; mandatory for derived themes.
+   (modeline-fg          fg)
+   (modeline-fg-alt      base4)
+   (modeline-bg          (if doom-molokai-brighter-modeline base4 base3))
+   (modeline-bg-inactive (doom-darken (if doom-molokai-brighter-modeline
+                                          base3
+                                        base2)
+                                      0.2))
+   (org-quote `(,(doom-lighten (car bg) 0.05) "#1f1f1f"))
+
+   (-modeline-pad
+    (when doom-molokai-padded-modeline
+      (if (integerp doom-molokai-padded-modeline) doom-molokai-padded-modeline 4))))
+
+
+  ;;;; Base theme face overrides
+  ((cursor :background magenta)
+   (lazy-highlight :background violet :foreground base0 :distant-foreground base0 :bold bold)
+   ((line-number &override) :foreground base5 :distant-foreground nil)
+   ((line-number-current-line &override) :foreground base7 :distant-foreground nil)
+   (mode-line
+    :background modeline-bg :foreground modeline-fg
+    :box (if -modeline-pad `(:line-width ,-modeline-pad :color modeline-bg)))
+   (mode-line-inactive
+    :background modeline-bg-inactive :foreground modeline-fg-alt
+    :box (if -modeline-pad `(:line-width ,-modeline-pad :color modeline-bg-inactive)))
+   (isearch :foreground base0 :background green)
+
+   ;;;; centaur-tabs
+   (centaur-tabs-active-bar-face :background green)
+   (centaur-tabs-modified-marker-selected :inherit 'centaur-tabs-selected :foreground green)
+   (centaur-tabs-modified-marker-unselected :inherit 'centaur-tabs-unselected :foreground green)
+   ;;;; css-mode <built-in> / scss-mode
+   (css-proprietary-property :foreground keywords)
+   ;;;; doom-modeline
+   (doom-modeline-bar :background green)
+   (doom-modeline-buffer-file :inherit 'mode-line-buffer-id :weight 'bold)
+   (doom-modeline-buffer-path :inherit 'bold :foreground green)
+   (doom-modeline-buffer-project-root :foreground green :weight 'bold)
+   (doom-modeline-buffer-modified :inherit 'bold :foreground orange)
+   ;;;; ediff <built-in>
+   (ediff-fine-diff-A :background (doom-blend magenta bg 0.3) :weight 'bold)
+   ;;;; evil
+   (evil-search-highlight-persist-highlight-face :background violet)
+   ;;;; evil-snipe
+   (evil-snipe-first-match-face :foreground base0 :background green)
+   (evil-snipe-matches-face     :foreground green :underline t)
+   ;;;; flycheck
+   (flycheck-error   :underline `(:style wave :color ,red)    :background base3)
+   (flycheck-warning :underline `(:style wave :color ,yellow) :background base3)
+   (flycheck-info    :underline `(:style wave :color ,green)  :background base3)
+   ;;;; helm
+   (helm-swoop-target-line-face :foreground magenta :inverse-video t)
+   ;;;; ivy
+   (ivy-current-match :background base3)
+   (ivy-minibuffer-match-face-1 :background base1 :foreground base4)
+   ;;;; markdown-mode
+   (markdown-blockquote-face :inherit 'italic :foreground dark-blue)
+   (markdown-list-face :foreground magenta)
+   (markdown-pre-face  :foreground cyan)
+   (markdown-link-face :inherit 'bold :foreground blue)
+   ((markdown-code-face &override) :background (doom-lighten base2 0.045))
+   ;;;; neotree
+   (neo-dir-link-face   :foreground cyan)
+   (neo-expand-btn-face :foreground magenta)
+   ;;;; outline <built-in>
+   ((outline-1 &override) :foreground magenta)
+   ((outline-2 &override) :foreground orange)
+   ;;;; org <built-in>
+   (org-ellipsis :foreground orange)
+   (org-tag :foreground yellow :bold nil)
+   ((org-quote &override) :foreground base7)
+   (org-todo :foreground yellow :bold 'inherit)
+   (org-list-dt :foreground yellow)
+   ;;;; rainbow-delimiters
+   (rainbow-delimiters-depth-1-face :foreground magenta)
+   (rainbow-delimiters-depth-2-face :foreground orange)
+   (rainbow-delimiters-depth-3-face :foreground green)
+   (rainbow-delimiters-depth-4-face :foreground cyan)
+   (rainbow-delimiters-depth-5-face :foreground magenta)
+   (rainbow-delimiters-depth-6-face :foreground orange)
+   (rainbow-delimiters-depth-7-face :foreground green))
+
+  ;;;; Base theme variable overrides
+  ;; ()
+  )
+
+;;; doom-molokai-theme.el ends here
