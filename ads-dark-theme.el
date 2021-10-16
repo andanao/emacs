4353b462352d2b999b6b116990262f5f9bdf0fb5
Adrian Danao-Schroeder <47489123+andanao@users.noreply.github.com>
2021-10-15 23:20:09 -0400

    Kinda happy with this for now

--- ads-dark-theme.el
+++ ads-dark-theme.el
@@ -99,8 +99,6 @@ Can be an integer to determine the exact padding."
                                           base3
                                         base2)
                                       0.2))
-   (org-quote `(,(doom-lighten (car bg) 0.05) "#1f1f1f"))
-
    (-modeline-pad
     (when ads-dark-padded-modeline
       (if (integerp ads-dark-padded-modeline) ads-dark-padded-modeline 4))))
@@ -109,8 +107,8 @@ Can be an integer to determine the exact padding."
   ;;;; Base theme face overrides
   ((cursor :background magenta)
    (lazy-highlight :background violet :foreground base0 :distant-foreground base0 :bold bold)
-   ((line-number &override) :foreground base5 :distant-foreground nil)
-   ((line-number-current-line &override) :foreground base7 :distant-foreground nil)
+   (line-number :foreground base5 )
+   (line-number-current-line :foreground magenta )
    (mode-line
     :background modeline-bg :foreground modeline-fg
     :box (if -modeline-pad `(:line-width ,-modeline-pad :color modeline-bg)))
@@ -162,34 +160,50 @@ Can be an integer to determine the exact padding."
    ;;;; org <built-in>
    (org-ellipsis :foreground orange)
    (org-tag :foreground yellow :bold nil)
-   ((org-quote &override) :foreground base7)
-   (org-todo :foreground yellow :bold 'inherit)
+   (org-quote :height 2.8 :font serif :foreground fg :background bg)
+   (org-todo :foreground yellow :bold 'inherit :font sans)
+   (org-done :inherit '(fixed-pitch org-headline-done))
    (org-list-dt :foreground yellow)
    ;;;; rainbow-delimiters
    (rainbow-delimiters-depth-1-face :foreground magenta)
    (rainbow-delimiters-depth-2-face :foreground orange)
    (rainbow-delimiters-depth-3-face :foreground green)
    (rainbow-delimiters-depth-4-face :foreground cyan)
-   (rainbow-delimiters-depth-5-face :foreground magenta)
-   (rainbow-delimiters-depth-6-face :foreground orange)
-   (rainbow-delimiters-depth-7-face :foreground green)
+   (rainbow-delimiters-depth-5-face :foreground violet)
+   (rainbow-delimiters-depth-6-face :foreground dark-cyan)
+   (rainbow-delimiters-depth-7-face :foreground red)
    ;;;; 
-   ;; (org-block     :foreground nil  :inherit 'fixed-pitch)
-   (org-table     :inherit 'fixed-pitch)
+   ((org-block &override)    :background base1 :inherit 'fixed-pitch)
+   (org-block-begin :background bg :height 0.8)
+   
+   (org-table     :foreground dark-cyan :inherit 'fixed-pitch :background bg)
    (org-formula   :inherit 'fixed-pitch)
-   (org-code      :inherit '(shadow fixed-pitch))
-   (org-table     :inherit '(shadow fixed-pitch))
-   (org-verbatim  :inherit '(shadow fixed-pitch))
+   (org-code      :foreground dark-cyan :inherit 'fixed-pitch)
+   (org-verbatim  :font serif :height 1.2 :slant 'italic :foreground base8)
+
+   
    (org-special-keyword  :inherit '(font-lock-comment-face fixed-pitch))
    (org-meta-line  :inherit '(font-lock-comment-face fixed-pitch))
-   (org-checkbox   :inherit 'fixed-pitch)
-   (line-number  :inherit 'fixed-pitch)
+   (org-checkbox   :height 0.8 :inherit 'fixed-pitch)
+   (org-checkbox-statistics-todo :foreground orange :inherit 'org-checkbox)
+   (org-checkbox-statistics-done :foreground base5 :inherit 'org-checkbox)
+   (line-number    :inherit 'fixed-pitch)
    (line-number-current-line  :inherit 'fixed-pitch)
-   (org-document-title :weight 'bold :height 3.0) 
+   (org-document-title :height 3.0) 
+   (org-level-1 :height 1.8 :foreground base8 )
+   (org-level-2 :height 1.6 :foreground base8 )
+   (org-level-3 :height 1.4 :foreground base8 )
+   (org-level-4 :height 1.3 :foreground base8 )
+   (org-level-5 :height 1.2 :foreground base8 )
+   (org-level-6 :height 1.1 :foreground base8 )
+   (org-level-7 :height 1.1 :foreground base8 )
+   (org-level-8 :height 1.1 :foreground base8 )
 
+   (org-drawer         :foreground base5 :inherit 'fixed-pitch)
+   (org-property-value :foreground dark-cyan :inherit 'fixed-pitch)
+   (org-date :foreground orange  :inherit 'fixed-pitch)
 
-
-
+   (ivy-org :font serif :height 1.2 :weight 'bold)
    )
 
   ;;;; Base theme variable overrides
