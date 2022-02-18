8e31d35f31846c2842eb15a31434d6f5336cdc62
adrian-xps-ubuntu <adriandanao@gmail.com>
2022-02-17 22:30:43 -0500

    init working again

--- init.el
+++ init.el
@@ -8,9 +8,9 @@
 
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
                          ("gnu" . "https://elpa.gnu.org/packages")
-                         ("nongnu" . "https://elpa.nongnu.org/nongnu")
+                         ("nongnu" . "https://elpa.nongnu.org/nongnu/")
                          ("org" . "https://orgmode.org/elpa/")
-                         ;; ("elpa" . "https://elpa.gnu.org/packages/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")
 			 ))
 
 (package-initialize)
