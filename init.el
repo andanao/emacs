2c299ed18ecbba68c68754074b0ade5fc743e5ba
adrian-xps-ubuntu <adriandanao@gmail.com>
2022-02-22 08:50:07 -0500

    emacs backup directory

--- init.el
+++ init.el
@@ -46,6 +46,14 @@
 (menu-bar-mode -1)                      ; Disable the menu bar
 (set-fringe-mode 10)                    ; Add some breathing room
 
+(setq backup-directory-alist '(("." . "~/.emacs.d/backup"))
+  backup-by-copying t    ; Don't delink hardlinks
+  version-control t      ; Use version numbers on backups
+  delete-old-versions t  ; Automatically delete excess backups
+  kept-new-versions 20   ; how many of the newest versions to keep
+  kept-old-versions 5    ; and how many of the old
+  )
+
 (setq mono "Fira Code Retina")
 (setq sans "Cantarell")
 (setq serif "EtBembo")
