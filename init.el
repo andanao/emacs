7cd26b5f2201ede3d0c7f947f4e5b751c5ae69be
adrian-xps-ubuntu <adriandanao@gmail.com>
2022-02-23 21:26:36 -0500

    treemacs remove winum keymap

--- init.el
+++ init.el
@@ -1065,9 +1065,8 @@ are tangled."
 (use-package treemacs
   :ensure t
   :defer t
-  :init
-  (with-eval-after-load 'winum
-    (define-key winum-keymap (kbd "M-0") #'treemacs-select-window))
+  ;; :init
+  
   :config
   (progn
     (setq treemacs-collapse-dirs                 (if treemacs-python-executable 3 0)
